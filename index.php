ee875b22bbb54b108788069e48dafc76a70d4fff
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-19 11:57:23 +0900

    バリデーション追加

--- index.php
+++ index.php
@@ -3,28 +3,47 @@
 $con = new PDO('mysql:host=localhost;dbname=mybbs;charset=utf8','appuser','eDZNQ7ZnMuDm');
 
 // 投稿をDBへ書き込み
-if (isset($_POST['form_post']) == true) {
-  $name = $_POST[name];
-  $form_body = $_POST[form_body];
+if (isset($_POST['form_post']) == true) 
+{
+  $name = $_POST['name'];
+  $form_body = $_POST['form_body'];
   $post_date = date('Y年m月d日 H:i');
-
-  if ($name !== '' && $form_body !== '') {
-    $stm = $con->prepare("INSERT INTO post(name,form_body,post_date) values(:name,:form_body,:post_date)");
-
-    $stm->bindValue(':name', $name);
-    $stm->bindValue(':form_body', $form_body);
-    $stm->bindValue(':post_date', $post_date);
-    $result = $stm->execute();
-
-    if ($result == true) {
-      $msg = "書き込みが完了しました。";
-      } else {
-        $err_msg ="書き込みに失敗しました。";
-      }
-  } else {
-    $err_msg = "名前と本文を入力してください。"; 
-    }
 }
+
+function validation($name, $form_body) 
+{
+  if ($name == '' || $form_body == '') {
+    $result = false;
+  } else {
+    $result = true;
+  }
+
+  if($result == true)
+  {
+    return true;
+  }
+}
+
+$validate = validation($name, $form_body);
+
+if ($validate == true) {
+  $stm = $con->prepare("INSERT INTO post(name,form_body,post_date) values(:name,:form_body,:post_date)");
+
+  $stm->bindValue(':name', $name);
+  $stm->bindValue(':form_body', $form_body);
+  $stm->bindValue(':post_date', $post_date);
+  $result = $stm->execute();
+
+  if ($result == true) {
+    $msg = "書き込みが完了しました。";
+    } else {
+      $err_msg ="書き込みに失敗しました。";
+    }
+} else {
+  $err_msg = "名前と本文を入力してください。"; 
+  }
+
+echo $err_msg;
   
 // 投稿一覧を出力
 $sth = $con->prepare("SELECT * FROM post ORDER BY id DESC");
@@ -32,71 +51,68 @@ $sth = $con->prepare("SELECT * FROM post ORDER BY id DESC");
 $sth->execute();
 
 $output = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>井上のBBS</title>
-  <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
-  <link rel="stylesheet" href="common.css">
-</head>
-<body>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-  <script src="js/reply.js"></script>
-  <div id="all">
-    <div class="row wrapper">
-      <h1 id="form-ttl">井上のBBS</h1>
-      <div class="form-wrap">
-        <form action="index.php" method="post" accept-charset="utf-8">
-          <h2 class="sub-ttl">投稿フォーム</h2>
-            <div class="text-box form-group">
-              <span class="form-item">名前</span>
-              <input class="form-text form-control" type="text" name="name"><br>
-            </div>  
-            <div class="text-box form-group">
-              <span class="form-item">本文</span><br>
-              <textarea class="form-body" name="form_body" cols="68" rows="10"></textarea><br>
-              <span class="post-btn">
-                <input class="form-text" type="submit" name="form_post" value="投稿">
-              </span>
-            </div>
-        </form>  
-      </div>
-      <h2 class="sub-ttl">投稿一覧</h2>      
-      <?php 
-        for ($i=0; $i < count($output); $i++) { 
-        echo '<div class="post-list"><span id="'.$output[$i]['id'].'"class="post-number">No:'.$output[$i]['id'].'<br><span class="post-name">名前:'.$output[$i]['name'].'</span><span class="post-date">'.$output[$i]['post_date'].'</span><br><p class="post-body">'.$output[$i]['form_body'].'</p><br><a href="reply.php"><span class="reply">返信する</span></a>';
-        $num_id = $output[$i]['id'];
-
-        }
-      ?>
-      <div class="form-wrap">
-        <form action="index.php" method="post" accept-charset="utf-8">
-          <h2 class="sub-ttl">投稿フォーム</h2>
-            <div class="text-box form-group">
-              <span class="form-item">名前</span>
-              <input class="form-text form-control" type="text" name="name"><br>
-            </div>  
-            <div class="text-box form-group">
-              <span class="form-item">本文</span><br>
-              <textarea class="form-body" name="form_body" cols="68" rows="10"></textarea><br>
-              <span class="post-btn">
-                <input class="form-text" type="submit" name="form_post" value="投稿">
-              </span>
-            </div>
-        </form>  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>井上のBBS</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="common.css">
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- <script src="js/reply.js"></script> -->
+    <div id="all">
+      <div class="row wrapper">
+        <h1 id="form-ttl">井上のBBS</h1>
+        <div class="form-wrap">
+          <form action="index.php" method="post" accept-charset="utf-8">
+            <h2 class="sub-ttl">投稿フォーム</h2>
+              <div class="text-box form-group">
+                <span class="form-item">名前</span>
+                <input class="form-text form-control" type="text" name="name"><br>
+              </div>  
+              <div class="text-box form-group">
+                <span class="form-item">本文</span><br>
+                <textarea class="form-body" name="form_body" cols="68" rows="10"></textarea><br>
+                <span class="post-btn">
+                  <input class="form-text" type="submit" name="form_post" value="投稿">
+                </span>
+              </div>
+          </form>  
+        </div>
+        <h2 class="sub-ttl">投稿一覧</h2>      
+        <?php 
+          for ($i=0; $i < count($output); $i++) { 
+          echo '<div class="post-list"><span id="'.$output[$i]['id'].'"class="post-number">No:'.$output[$i]['id'].'<br><span class="post-name">名前:'.$output[$i]['name'].'</span><span class="post-date">'.$output[$i]['post_date'].'</span><br><p class="post-body">'.$output[$i]['form_body'].'</p><br><a href="reply.php"><span class="reply">返信する</span></a>';
+          $num_id = $output[$i]['id'];
+          }
+        ?>
+        <div class="form-wrap">
+          <form action="index.php" method="post" accept-charset="utf-8">
+            <h2 class="sub-ttl">投稿フォーム</h2>
+              <div class="text-box form-group">
+                <span class="form-item">名前</span>
+                <input class="form-text form-control" type="text" name="name"><br>
+              </div>  
+              <div class="text-box form-group">
+                <span class="form-item">本文</span><br>
+                <textarea class="form-body" name="form_body" cols="68" rows="10"></textarea><br>
+                <span class="post-btn">
+                  <input class="form-text" type="submit" name="form_post" value="投稿">
+                </span>
+              </div>
+          </form>  
+        </div>
       </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
 
           <!-- 返信ボタン
