ffef1c5a720414c1363b60433d246717bdd48837
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-24 19:21:09 +0900

    twigでdump()を使えるよう設定

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ error_reporting(E_ALL);
 // twig
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array(
+  'debug' => true,
+  ));
+$twig->addExtension(new Twig_Extension_Debug());
 
 
 session_start();
