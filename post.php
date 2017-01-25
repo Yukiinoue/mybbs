5d31ef62a9ecfc78bad1a2f02630c6ffe0ec752b
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-25 19:26:50 +0900

    子記事に対応する親記事IDのDB保存実装途中

--- post.php
+++ post.php
@@ -5,7 +5,7 @@ require "validation.php";
 
 $con = new PDO('mysql:host=localhost;dbname=mybbs;charset=utf8','appuser','eDZNQ7ZnMuDm');
 
-// 投稿をDBへ書き込み
+// フォームの入力内容を変数に格納
 if (isset($_POST['form_post']) === true) 
 {
   $name = $_POST['name'];
@@ -13,9 +13,13 @@ if (isset($_POST['form_post']) === true)
   $post_date = date('Y年m月d日 H:i');
   $password = $_POST['password'];
 }
+if (isset($_POST['parent_id']))
+{
+  $parent_id = $_POST['parent_id'];
+}
 
+// バリデーションメッセージの格納
 $validate = validation($name, $form_body);
-
 $_SESSION['result'] = $validate;
 
 
@@ -27,7 +31,15 @@ if ($validate === true) {
   $stm->bindValue(':form_body', $form_body);
   $stm->bindValue(':post_date', $post_date);
   $stm->bindValue(':password', $password);
+
   $result = $stm->execute();
+
+  if (isset($parent_id)) {
+    $stm = $con->prepare("INSERT INTO post(reply_id) values (:reply_id)");
+    $stm->bindValue(':reply_id', $parent_id);
+    $result = $stm->execute();
+  }
 }
 
-header("Location: /mybbs/index.php");
+
+  header("Location: /mybbs/index.php");
