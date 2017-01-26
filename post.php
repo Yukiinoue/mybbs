7643b589c8f0a5c7be3c1e459efc0f38432f1f40
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-26 11:00:30 +0900

    parent_id 初期値設定

--- post.php
+++ post.php
@@ -7,13 +7,14 @@ require "db.php";
 $con = db_connect();
 
 // フォームの入力内容を変数に格納
+$parent_id = null;
 $name = $_POST['name'];
 $form_body = $_POST['form_body'];
 $post_date = date('Y年m月d日 H:i');
 $password = $_POST['password'];
 
 if (isset($_POST['parent_id'])) {
-  $parent_id = $_POST['parent_id'];  
+  $parent_id = $_POST['parent_id'];
 }
 
 
