0f28e7dc3405774866c9582a0803836dff98207f
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-26 19:42:30 +0900

    bootstrap削除とレイアウト調整

--- js/reply.js
+++
@@ -1,8 +0,0 @@
-<script>
-  $('#sampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var recipient = button.data('recipient');
-    var modal = $(this);
-    modal.find('.modal-title').text(recipient + 'へのメッセージ');
-  });
-</script>
