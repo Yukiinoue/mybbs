b7ddea88b0caa324d947581e511291aed596db79
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-01-26 11:11:07 +0900

    DB保存記述の修正

--- post.php
+++ post.php
@@ -25,27 +25,19 @@ $_SESSION['result'] = $validate;
 
 // 投稿をDBへ保存 
 if ($validate === true) {
-  if (isset($_POST['form_post']) === true) {
-  $stm = $con->prepare("INSERT INTO post(name,form_body,post_date,password) values(:name,:form_body,:post_date,:password)");
-
-  $stm->bindValue(':name', $name);
-  $stm->bindValue(':form_body', $form_body);
-  $stm->bindValue(':post_date', $post_date);
-  $stm->bindValue(':password', $password);
-
-  $result = $stm->execute();
-  } elseif (isset($_POST['reply_post']) === true)
-  {
-  $stm = $con->prepare("INSERT INTO post(reply_id,name,form_body,post_date,password) values(:reply_id,:name,:form_body,:post_date,:password)");
-
-  $stm->bindValue(':reply_id', $parent_id);
-  $stm->bindValue(':name', $name);
-  $stm->bindValue(':form_body', $form_body);
-  $stm->bindValue(':post_date', $post_date);
-  $stm->bindValue(':password', $password);
-
-  $result = $stm->execute();
+  if (isset($_POST['parent_id'])) {
+    $stm = $con->prepare("INSERT INTO post(reply_id,name,form_body,post_date,password) values(:reply_id,:name,:form_body,:post_date,:password)");
+    $stm->bindValue(':reply_id', $parent_id);
+  } else {
+    $stm = $con->prepare("INSERT INTO post(name,form_body,post_date,password) values(:name,:form_body,:post_date,:password)");
   }
+
+  $stm->bindValue(':name', $name);
+  $stm->bindValue(':form_body', $form_body);
+  $stm->bindValue(':post_date', $post_date);
+  $stm->bindValue(':password', $password);
+
+  $result = $stm->execute();
 }
 
   header("Location: /mybbs/index.php");
