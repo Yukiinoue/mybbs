8b3908fcec02125bc8e6ac606b3a6d38e3413cf4
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-02-02 20:00:07 +0900

    子記事返信ループ修正（途中経過）

--- index.php
+++ index.php
@@ -21,14 +21,17 @@ if (isset($_SESSION['result'])) {
     unset($_SESSION['result']);
 }
 
+
 // 投稿記事の取得
-function get_post($con)
+
+function get_post($con, $reply_id = 0)
 {
 // 初期化
 $output = array();
 
 // 一覧データの取得
-$sth = $con->prepare("SELECT * FROM post WHERE reply_id=0 ORDER BY id DESC");
+$sth = $con->prepare("SELECT * FROM post WHERE reply_id = :reply_id ORDER BY id DESC");
+$sth->bindValue(':reply_id', $reply_id);
 $sth->execute();
 $output = $sth->fetchAll(PDO::FETCH_ASSOC);
 
@@ -43,17 +46,20 @@ function get_tree($con, $parent_posts)
     foreach ($parent_posts as $key=>$post)
     {
         // 親記事に対する返信一覧を取得し、childrenへ挿入
-        $post[$key]['children'] = get_post($con);
+        $post[$key]['children'] = get_post($con, $post['id']);
 
         // 取得したchildrenを親とした返信一覧の取得し、ループ
         $post[$key]['children'] = get_tree($con, $post[$key]['children']);
     }
 
     // ツリー型一覧データの返却
-    return $post_data;
+    return $post;
 }
 
-$post_data = get_tree($con, $output);
+$reply = get_tree($con, $output);
+var_dump($reply);
+
+
 // templateの出力
 $view = 'index.html';
 twig_view ($view, $output, $message);
