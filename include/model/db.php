c965d13e14e25b5735072993260f0c0332d7354e
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-02-03 13:10:10 +0900

    記事取得関数の切り出し

--- include/model/db.php
+++ include/model/db.php
@@ -1,6 +1,48 @@
 <?php
-  function db_connect ()
-  {
-    $con = new PDO('mysql:host=192.168.33.10;dbname=mybbs;charset=utf8','appuser','eDZNQ7ZnMuDm');
+// DB接続
+function db_connect ()
+{
+    $con = new PDO('mysql:host=localhost;dbname=mybbs;charset=utf8','appuser','eDZNQ7ZnMuDm');
     return $con;
-  }
+}
+
+// DB接続
+$con = db_connect();
+
+// 投稿記事の取得
+function get_post($con, $reply_id = 0)
+{
+// 初期化
+$output = array();
+
+// 一覧データの取得
+$sth = $con->prepare("SELECT * FROM post WHERE reply_id = :reply_id ORDER BY id DESC");
+$sth->bindValue(':reply_id', $reply_id);
+$sth->execute();
+$output = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+return $output;
+}
+
+$output = get_post($con);
+
+// ツリー型一覧データの形成
+function get_tree($con, $parent_posts)
+{
+    $post = array();
+    foreach ($parent_posts as $key=>$post)
+    {
+
+        // 親記事に対する返信一覧を取得
+        $posts = get_post($con, $post['id']);
+
+        // 取得したchildrenを親とした返信一覧の取得し、ループ
+        $post[$key]['children'] = get_tree($con, $posts);
+    }
+
+    // ツリー型一覧データの返却
+    return $post;
+}
+
+
+$reply = get_tree($con, $output);
--- index.php
+++ index.php
@@ -10,9 +10,6 @@ session_start();
 require "include/conf/twig.php";
 require "include/model/db.php";
 
-// DB接続
-$con = db_connect();
-
 // バリデーションメッセージの受け取り、変数格納
 $message = null;
 
@@ -20,46 +17,6 @@ if (isset($_SESSION['result'])) {
     $message = $_SESSION['result'];
     unset($_SESSION['result']);
 }
-
-
-// 投稿記事の取得
-
-function get_post($con, $reply_id = 0)
-{
-// 初期化
-$output = array();
-
-// 一覧データの取得
-$sth = $con->prepare("SELECT * FROM post WHERE reply_id = :reply_id ORDER BY id DESC");
-$sth->bindValue(':reply_id', $reply_id);
-$sth->execute();
-$output = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-return $output;
-}
-
-$output = get_post($con);
-
-// ツリー型一覧データの形成
-function get_tree($con, $parent_posts)
-{
-    $post = array();
-    foreach ($parent_posts as $key=>$post)
-    {
-        // 親記事に対する返信一覧を取得し、childrenへ挿入
-        $post[$key]['children'] = get_post($con, $post['id']);
-
-        // 取得したchildrenを親とした返信一覧の取得し、ループ
-        $post[$key]['children'] = get_tree($con, $post[$key]['children']);
-    }
-
-    // ツリー型一覧データの返却
-    return $post;
-}
-
-$reply = get_tree($con, $output);
-var_dump($reply);
-
 
 // templateの出力
 $view = 'index.html';
