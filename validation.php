b79d04e2f62aca175b34d57d8883b710c6ae1398
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-02-16 14:50:00 +0900

    編集専用のバリデーションを追加

--- validation.php
+++ validation.php
@@ -21,3 +21,27 @@ function validation($name, $form_body)
         return $error_msg;
     }
 }
+
+// 編集フォームのバリデーション
+// 後に、validation関数と統合できたら統合
+function edit_validation($form_body, $password)
+{
+    $result = true;
+    $error_msg = [];
+
+    if($password == '') {
+        $result = false;
+        $error_msg[] = 'パスワードが入力されていません。';
+    }
+
+    if ($form_body == '') {
+        $result = false;
+        $error_msg[] = '本文が入力されていません。';
+    }
+
+    if($result == true) {
+        return $result;
+    } elseif ($result == false) {
+        return $error_msg;
+    }
+}
