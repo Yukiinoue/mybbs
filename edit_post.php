c25c80a9171dd3b7f7116a2876b02f3e0c3d1f07
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-02-16 14:51:15 +0900

    記事編集機能の実装

---
+++ edit_post.php
@@ -0,0 +1,56 @@
+<?php
+// エラー表示
+ini_set("display_errors", "On");
+error_reporting(E_ALL);
+
+session_start();
+
+require "validation.php";
+require "include/model/db.php";
+
+$con = db_connect();
+
+// フォームの入力内容を変数に格納
+$parent_id = $_POST['post_id'];
+$form_body = $_POST['form_body'];
+$post_date = date('Y年m月d日 H:i');
+$password = $_POST['password'];
+
+
+// バリデーションメッセージの格納
+$validate = edit_validation($form_body, $password);
+$_SESSION['result'] = $validate;
+
+// 編集対象記事のパスワードを取得
+if ($validate === true) {
+    if (isset($_POST['post_id'])) {
+        $parent_password = array();
+        $sth = $con->prepare("SELECT password FROM post WHERE id = :id");
+        $sth->bindValue('id', $parent_id);
+
+        $sth->execute();
+
+        $parent_password = $sth->fetch(PDO::FETCH_ASSOC);
+        $parent_password = $parent_password['password'];
+
+     }
+}
+
+// パスワードが一致しているかどうかの判定
+if ($password == $parent_password) {
+    // 記事本文の更新
+    $stm = $con->prepare("UPDATE post SET form_body=:form_body WHERE id = :id");
+    $stm->bindValue(':form_body', $form_body);
+    $stm->bindValue(':id', $parent_id);
+
+    $stm->execute();
+
+    header("Location: /mybbs/index.php");
+    exit();
+} else {
+    // エラーメッセージの表示
+    $error_msg[] = 'パスワードが間違っています。';
+    $_SESSION['result'] = $error_msg;
+    header("Location: /mybbs/edit.php");
+    exit();
+}
