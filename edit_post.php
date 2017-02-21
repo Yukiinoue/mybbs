97bc000c45650f94bd16aa705a52099c0d05b84b
井上 祐樹 <yuki.inoue@befools-MacBook-Air.local>
2017-02-21 11:03:02 +0900

    post_id, 記事がない時のエラー処理修正

--- edit_post.php
+++ edit_post.php
@@ -13,36 +13,28 @@ require "include/model/db.php";
 // DB接続
 $con = db_connect();
 
-// post_idのチェック
 // リクエスト値の有無
-if (isset($_REQUEST)) {
-    if (isset($_POST['post_id'])) {
-      // フォームの入力内容を変数に格納
-      $parent_id = $_POST['post_id'];
-      $form_body = $_POST['form_body'];
-      $post_date = date('Y年m月d日 H:i');
-      $password = $_POST['password'];
 
-      $check = true;
-    } else {
-        $check = false;
-    }
-} else {
-    $check = false;
+// post_idのチェック
+$parent_id = isset($_POST['post_id']) ? $_POST['post_id'] : null;
+if (! $_POST['post_id']) {
+    die('post_idがありません。');
 }
 
-// $checkのtrue or falseでの条件分岐
-if ($check === true) {
-    // バリデーションメッセージの格納
-    $validate = edit_validation($form_body, $password);
-    $_SESSION['result'] = $validate;
-} elseif($check === false) {
-    // post_idのチェックがfalseの場合の画面遷移
-    $error_msg[] = '不正なidです。';
-    $_SESSION['result'] = $error_msg;
-    header("Location: /mybbs/index.php");
-    exit();
+$post = get_parent($con, $parent_id);
+
+if (! $post) {
+    die('記事がありません。');
 }
+
+// フォームの入力内容を変数に格納
+$form_body = $_POST['form_body'];
+$post_date = date('Y年m月d日 H:i');
+$password = $_POST['password'];
+
+// バリデーションメッセージの格納
+$validate = edit_validation($form_body, $password);
+$_SESSION['result'] = $validate;
 
 // 編集対象記事のパスワードを取得
 if ($validate === true) {
