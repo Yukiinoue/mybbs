ed3da1155f41fae965e7de493d4fa542dca8f9ff
Yukiinoue <yuki.inoue@befool.co.jp>
2017-02-24 11:47:37 +0900

    記述の簡素化

--- delete.php
+++ delete.php
@@ -4,51 +4,40 @@ ini_set("display_errors", "On");
 error_reporting(E_ALL);
 
 session_start();
+
 // 別ファイルの呼び出し
 require 'vendor/autoload.php';
 require 'include/conf/twig.php';
 require 'include/model/db.php';
 
+// DB接続
 $con = db_connect();
 
 // バリデーションメッセージの受け取り、変数格納
 $message = null;
+
 if (isset($_SESSION['result'])) {
     $message = $_SESSION['result'];
     unset($_SESSION['result']);
 }
 
-// 編集対象の親記事のid取得
+// 削除対象の親記事のid取得
 $error_msg = array();
 
-if (isset($_POST['post_id'])) {
-    $parent_id = $_POST['post_id'];
-} elseif(isset($_SESSION['post_id'])){
-    $parent_id = $_SESSION['post_id'];
-} elseif(empty($_POST['post_id'])) {
-    $error_msg[] = '不正なidです。';
-    $_SESSION['result'] = $error_msg;
-    header("Location: index.php");
-    exit();
+$parent_id = isset($_REQUEST['post_id']) ? $_REQUEST['post_id'] : null;
+if (! $parent_id) {
+    die('post_idがありません。');
 }
 
-// 編集対象の親記事とバリデーションメッセージの格納
+// 削除対象の親記事とバリデーションメッセージの格納
 $data = array();
 
 $data['parent_post'] = get_parent($con, $parent_id);
 $data['message'] = $message;
 
-if (empty($data['parent_post'])) {
-    $error_msg[] = '削除対象記事がありません。';
-    $_SESSION['result'] = $error_msg;
-    header("Location: index.php");
-    exit();
+if (! $data['parent_post']) {
+    die('削除対象記事がありません。');
 }
-
-// 削除対象記事の取得
-$data = array();
-$data['parent_post'] = get_parent($con, $parent_id);
-$data['message'] = $message;
 
 // templateの出力
 $view = 'delete.html';
