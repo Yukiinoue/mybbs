ec9fd92dd49bcbe628d3409e7c983d65ecc9cefc
Yukiinoue <yuki.inoue@befool.co.jp>
2017-03-21 09:47:11 +0900

    データベースからセレクトするカラムの変更

--- file_load.php
+++ file_load.php
@@ -6,8 +6,8 @@ $con = db_connect();
 
 $id = isset($_GET['id'])? $_GET['id'] : null;
 
-$sth = $con->prepare("SELECT * FROM file WHERE `id` = :id");
-$sth->bindValue(':id', $id);
+$sth = $con->prepare("SELECT * FROM file WHERE `post_id` = :post_id");
+$sth->bindValue(':post_id', $id);
 $sth->execute();
 $files_data = $sth->fetch(PDO::FETCH_OBJ);
 
