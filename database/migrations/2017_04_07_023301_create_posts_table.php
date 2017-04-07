7906ad8d1d6f6b37c2f885e8e0100c0f8553a0af
Yukiinoue <yuki.inoue@befool.co.jp>
2017-04-07 12:02:06 +0900

    first_commit

---
+++ database/migrations/2017_04_07_023301_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reply_id');
+            $table->string('name');
+            $table->text('body');
+            $table->string('password');
+            $table->datetime('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
