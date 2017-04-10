babb309eb74d17e08f391116c77c07b0c6037936
Yukiinoue <yuki.inoue@befool.co.jp>
2017-04-10 11:20:09 +0900

    Ardentにてpostバリデーションの追加

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,21 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Ardent\Ardent;
 
-class Post extends Model
+class Post extends Ardent
 {
+    public static $rules = [
+        'name' => 'required',
+        'body' => 'required',
+        'password' => 'required|between: 4-32',
+        'posted_at' => 'required',
+    ];
+
     public function files()
     {
         return $this->hasMany('App\File');
     }
+
+
 }
