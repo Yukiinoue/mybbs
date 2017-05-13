885df059b2b8ac11a70d41305f18b44a3712e4cc
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-13 16:18:10 +0900

    フォームテンプレートの作成

---
+++ resources/views/form.blade.php
@@ -0,0 +1,17 @@
+{!! Form::open() !!}
+    <div class="form-group">
+        {!! Form::label('name', '名前:') !!}
+        {!! Form::text('name', null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('body', '本文:') !!}
+        {!! Form::textarea('body', null,['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('posted_at', '投稿日:') !!}
+        {!! Form::input('date', 'posted_at', date('Y-m-d'), ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Add Post',['class' => 'btn btn-primary form-control']) !!}
+    </div>
+{!! Form::close() !!}
