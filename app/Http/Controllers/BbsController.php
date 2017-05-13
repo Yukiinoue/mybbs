2ce21736dfc5c81b56b2d6c0c70d97b8ab08fed5
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-13 13:44:45 +0900

    最新の記事10件を取得

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Post;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controler;
+
 
 class BbsController extends Controller
 {
 	public function index()
 	{
-		$posts = Post::all();
+		// 最新記事10件の取得
+		$posts = DB::table('posts')->orderBy('id', 'desc')->take(10)->get();
 
 		return view('index', compact('posts'));
 	}
