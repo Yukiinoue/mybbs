3f57f6e119a31f170390e003d1f4506154cbce37
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-13 13:30:32 +0900

    テーブルの関連付けと入力禁止カラムの指定

--- app/Post.php
+++ app/Post.php
@@ -2,16 +2,32 @@
 
 namespace App;
 
-use \LaravelArdent\Ardent\Ardent;
+// use \LaravelArdent\Ardent\Ardent;
+use Illuminate\Database\Eloquent\Model;
 
-class Post extends Ardent
+class Post extends Model
 {
-    public static $rules = [
-        'name' => 'required',
-        'body' => 'required',
-        'password' => 'required|between: 4-32',
-        'posted_at' => 'required',
-    ];
+    /**
+     * モデルと関連しているテーブルの指定
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+
+    /**
+     * createメソッド実行時に、入力を禁止するカラムの指定
+     *
+     * @var array
+     */
+    protected $guarded = array('id');
+
+    // Ardentでバリデーションしたい
+    // public static $rules = [
+    //     'name' => 'required',
+    //     'body' => 'required',
+    //     'password' => 'required|between: 4-32',
+    //     'posted_at' => 'required',
+    // ];
 
     public function files()
     {
@@ -20,3 +36,4 @@ class Post extends Ardent
 
 
 }
+
