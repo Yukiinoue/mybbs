c6ba375569a63e80e43738b317247b3537aca0ea
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-13 15:03:41 +0900

    ページネーションの追加

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -11,8 +11,9 @@ class BbsController extends Controller
 	public function index()
 	{
 		// 最新記事10件の取得
-		$posts = DB::table('posts')->orderBy('id', 'desc')->take(10)->get();
+		$post_list = DB::table('posts')->orderBy('id', 'desc')->paginate(10);
+		$post_list->setPath('/');
 
-		return view('index', compact('posts'));
+		return view('index', compact('post_list'));
 	}
 }
