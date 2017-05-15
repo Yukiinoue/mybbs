d34d101498d5e45add3d150d100a6e886d516afb
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-15 19:11:52 +0900

    fillableのみの記述（一時対応）

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,7 @@ namespace App;
 
 // use \LaravelArdent\Ardent\Ardent;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -21,19 +22,29 @@ class Post extends Model
      */
     protected $guarded = array('id');
 
-    // Ardentでバリデーションしたい
-    // public static $rules = [
-    //     'name' => 'required',
-    //     'body' => 'required',
-    //     'password' => 'required|between: 4-32',
-    //     'posted_at' => 'required',
-    // ];
+    /**
+     * マスアサインメントで事前に登録できる項目の宣言
+     *
+     * @var array
+     */
+     protected $fillable = ['reply_id','name', 'body', 'password', 'posted_at'];
 
+    // ファイルとの関連性
     public function files()
     {
         return $this->hasMany('App\File');
     }
 
-
+    // 記事投稿メソッド
+    // static function create($request)
+    // {
+    //     $post = Post::create([
+    //         'reply_id' => '$reply_id',
+    //         'name' => $request['name'],
+    //         'body' => $request['body'],
+    //         'password' => $request['password'],
+    //         'posted_at' => Carbon::today(),
+    //     ]);
+    // }
 }
 
