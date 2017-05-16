baef1018caf56e96b6270ba08070908c90dc9afa
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-16 16:22:23 +0900

    編集テンプレートの作成

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>投稿編集画面</h1>
+
+    <hr/>
+
+@include('errors.form_errors')
+
+    {!! Form::model($post, ['method' => 'PATCH', 'url' => ['/', $post->id]]) !!}
+        @include('form', ['posted_at' => $post->posted_at->format('Y-m-d'), 'submitButton' => '編集'])
+    {!! Form::close() !!}
+@endsection
