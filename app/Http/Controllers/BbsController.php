abf06b734d66ef011f39841ab208ef23c4daab3f
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-16 16:28:01 +0900

    未来の日付での投稿をさせない処理

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controler;
+use Carbon\Carbon;
 
 
 class BbsController extends Controller
@@ -11,7 +12,7 @@ class BbsController extends Controller
 	public function index()
 	{
 		// 最新記事10件の取得
-		$post_list = DB::table('posts')->orderBy('id', 'desc')->paginate(10);
+		$post_list = DB::table('posts')->where('posted_at', '<=', Carbon::now())->orderBy('id', 'desc')->paginate(10);
 		$post_list->setPath('/');
 
 		return view('index', compact('post_list'));
