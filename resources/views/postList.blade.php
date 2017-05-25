a6145b8bfa03cdcd9bf924e1bcd80ecba058235f
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-25 19:00:27 +0900

    返信記事がない場合のメッセージ表示

--- resources/views/postList.blade.php
+++ resources/views/postList.blade.php
@@ -1,26 +1,30 @@
-@foreach($post_list as $post)
-    <article class="post_box clearfix">
-        <h2 class="name">
-            {{ $post->name }}
-        </h2>
-        <div class="body">
-            {{ $post->body }}
-        </div>
-        <br>
-        <div class="posted_at">
-            {{ $post->posted_at }}
-        </div>
-        <div class="btn-wrap">
-            {!! link_to(action('BbsController@replyPage', [$post->id]), '返信', ['class' => 'btn btn-info']) !!}
-        </div>
-        <div class="btn-wrap">
-            {!! link_to(action('BbsController@editPage', [$post->id]), '編集', ['class' => 'btn btn-primary']) !!}
-        </div>
-        <div class="btn-wrap">
-            {!! delete_form(['/', $post->id]) !!}
-        </div>
-        <div class="btn-wrap">
-            {!! link_to(action('BbsController@replyList', [$post->id]), 'この投稿の返信を見る', ['class' => 'btn btn-success']) !!}
-        </div>
-    </article>
-@endforeach
+@if($post_list->isEmpty())
+    <span>返信記事はありません</span>
+@else
+    @foreach($post_list as $post)
+        <article class="post_box clearfix">
+            <h2 class="name">
+                {{ $post->name }}
+            </h2>
+            <div class="body">
+                {{ $post->body }}
+            </div>
+            <br>
+            <div class="posted_at">
+                {{ $post->posted_at }}
+            </div>
+            <div class="btn-wrap">
+                {!! link_to(action('BbsController@replyPage', [$post->id]), '返信', ['class' => 'btn btn-info']) !!}
+            </div>
+            <div class="btn-wrap">
+                {!! link_to(action('BbsController@editPage', [$post->id]), '編集', ['class' => 'btn btn-primary']) !!}
+            </div>
+            <div class="btn-wrap">
+                {!! delete_form(['/', $post->id]) !!}
+            </div>
+            <div class="btn-wrap">
+                {!! link_to(action('BbsController@replyList', [$post->id]), 'この投稿の返信を見る', ['class' => 'btn btn-success']) !!}
+            </div>
+        </article>
+    @endforeach
+@endif
