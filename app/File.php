858d8c1db502014b34bf1bad0735d5e0492c0b84
Yukiinoue <yuki.inoue@befool.co.jp>
2017-05-26 12:26:03 +0900

    Fileモデルの実装

--- app/File.php
+++ app/File.php
@@ -2,17 +2,29 @@
 
 namespace App;
 
-use LaravelArdent\Ardent\Ardent;
+// use \LaravelArdent\Ardent\Ardent;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
-class File extends Ardent
+class File extends Model
 {
-    public static $rules = [
-        'post_id' => 'required',
-        'contents' => 'required',
-    ];
+
+    /**
+     * モデルと関連しているテーブルの指定
+     *
+     * @var string
+     */
+    protected $table = 'files';
+
+    /**
+     * createメソッド実行時に、入力を禁止するカラムの指定
+     *
+     * @var array
+     */
+    protected $guarded = array('id');
 
     public function post()
     {
-        return $this->belongsTo('App\Post');
+        return $this->belongsTo('App\Post','post_id', 'id')->orderBy('id', 'desc');
     }
 }
